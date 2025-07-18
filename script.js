9f79b0decd1bac12e38513a9f00b870c99d12cc2
Kinkin <161132585+kintarihashina@users.noreply.github.com>
2025-07-18 10:50:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let username;
 
 document.getElementById("mySubmit").onclick = function(){
- username = document.getElementById("myText").Value;
+ username = document.getElementById("myText").value;
  document.getElementById("myH2").textContent = `Hello ${username}`
 }
